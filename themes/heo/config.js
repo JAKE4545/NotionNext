5f5c1b751edb8321ec49569b97a389e39d7c6e9d
tingming.wu <tingming.wu@webeye.cn>
2026-01-05 22:16:32 +0800

    update config

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,7 +8,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://v5core.site' }
+    { title: '欢迎来到我的博客', url: 'https://blog.funqz.com' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -19,7 +19,7 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_LINK: 'https://v5core.site',
+  HEO_HERO_TITLE_LINK: 'https://blog.funqz.com',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
@@ -46,7 +46,7 @@ const CONFIG = {
   HEO_INFO_CARD_ICON1: 'fas fa-user',
   HEO_INFO_CARD_URL2: 'https://github.com/ALioooon',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://v5core.site',
+  HEO_INFO_CARD_URL3: 'https://blog.funqz.com',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -113,7 +113,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://v5core.site',
+  HEO_SOCIAL_CARD_URL: 'https://blog.funqz.com',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
